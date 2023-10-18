154177848b0ac942417c366b32caab826408e0c9
Prince Ibewiro <pibewiro@yahoo.com>
2023-10-18 16:41:32 -0300

    general adjustments

--- backend_wet_bat/src/server.ts
+++ backend_wet_bat/src/server.ts
@@ -4,7 +4,5 @@ import "./Infra/Database";
 
 import app from "./Infra/HttpServer";
 
-const PORT = process.env.API_PORT || 4000;
-app.listen(PORT, () =>
-  console.log(`Connected to port ${process.env.API_PORT}`)
-);
+const PORT = process.env.API_PORT ? Number(process.env.API_PORT) : 4000;
+app.listen(PORT, () => console.log(`Connected to port ${PORT}`));
